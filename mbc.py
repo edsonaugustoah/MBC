82e94a9854f8e79ba35f9a0953038deacc3b161b
edsonaugustoah <edsonaugustoah@gmail.com>
2023-12-23 09:58:13 -0300

    Nao cair se modbus nao conectar

--- mbc.py
+++ mbc.py
@@ -39,60 +39,59 @@ async def read_register(client, register):
         return None
 
 async def run_modbus_client():
-    # Criar cliente Modbus TCP assíncrono
-    client = ModbusClient.ModbusTcpClient(host, port=port)
+        
+    while True:    
+        
+        # Criar cliente Modbus TCP assíncrono
+        client = ModbusClient.ModbusTcpClient(host, port=port)
 
-    try:
-        # Conectar ao servidor Modbus
-        client.connect()
+        try:
+            # Conectar ao servidor Modbus
+            client.connect()
 
-        # Testar a conexão
-        if not client.is_socket_open():
-            raise Exception("A conexão com o servidor Modbus não foi estabelecida com sucesso.")
+            # Testar a conexão
+            if not client.is_socket_open():
+                raise Exception("A conexão com o servidor Modbus não foi estabelecida com sucesso.")
 
-        # Escrever nos registradores 1 e 2 do escravo com endereço 1
-        await write_registers(client, 1, 20)
-        await write_registers(client, 2, 23)
+            while True:
+                # Ler o valor do registrador 3 a cada 5 segundos
+                timestamp = int(time.time() * 1000)
+                registradores_ref = db.reference(f"Registradores/teste")
+                print("Conseguiu acessar")
 
-        while True:
-            # Ler o valor do registrador 3 a cada 5 segundos
-            timestamp = int(time.time() * 1000)
-            registradores_ref = db.reference(f"Registradores/teste")
-            print("Conseguiu acessar")
+                idRegistradores = registradores_ref.get()
+                if idRegistradores:
+                    registros = {}
 
-            idRegistradores = registradores_ref.get()
-            if idRegistradores:
-                registros = {}
+                    for register_number in idRegistradores:
+                        try:
+                            value = await read_register(client, int(register_number))
+                            print("value ",value)
+                            if value is not None:
+                                timestamp = int(time.time() * 1000)
+                                data = {
+                                    "data": value,
+                                    "time": timestamp
+                                }
+                                print(data)
+                                # Criar referência específica para o registrador atual
+                                register_ref = db.reference(f"Registros/teste/{register_number}")
+                                register_ref.update({timestamp: data})
+                        except Exception as e:
+                            print(f"Erro ao processar registrador {register_number}: {e}")
 
-                for register_number in idRegistradores:
-                    try:
-                        value = await read_register(client, int(register_number))
-                        print("value ",value)
-                        if value is not None:
-                            timestamp = int(time.time() * 1000)
-                            data = {
-                                "data": value,
-                                "time": timestamp
-                            }
-                            print(data)
-                            # Criar referência específica para o registrador atual
-                            register_ref = db.reference(f"Registros/teste/{register_number}")
-                            register_ref.update({timestamp: data})
-                    except Exception as e:
-                        print(f"Erro ao processar registrador {register_number}: {e}")
+                await asyncio.sleep(10)
 
-            await asyncio.sleep(10)
+        except Exception as e:
+            print(f"Erro durante a execução: {e}")
 
-    except Exception as e:
-        print(f"Erro durante a execução: {e}")
+        finally:
+            # Aguardar um curto período antes de fechar a conexão
+            await asyncio.sleep(0.1)
 
-    finally:
-        # Aguardar um curto período antes de fechar a conexão
-        await asyncio.sleep(0.1)
-
-        # Fechar a conexão se estiver aberta
-        if client.is_socket_open():
-            client.close()
+            # Fechar a conexão se estiver aberta
+            if client.is_socket_open():
+                client.close()
 
 if __name__ == "__main__":
     asyncio.run(run_modbus_client())
